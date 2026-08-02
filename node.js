a40380b72e644b70acdc0252543a2bee1bf2c020
unknown <pbhargavasai72@gmail.com>
2026-08-02 10:58:23 +0530

    again js updated

--- node.js
+++ node.js
@@ -2,3 +2,10 @@ console.log("Hello, World!");
 console.log("This is a Node.js application.");
 console.log("God is great");
 console.log("I am practicing JavaScriptg")
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+let result = addNumbers(5, 10);
+console.log(result); // Outputs: 15
