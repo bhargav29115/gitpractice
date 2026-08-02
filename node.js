02a48b55685499a4387895a6f6ff237427a2331a
unknown <pbhargavasai72@gmail.com>
2026-08-02 11:08:48 +0530

    Node.js

--- node.js
+++ node.js
@@ -1,7 +1,7 @@
 console.log("Hello, World!");
 console.log("This is a Node.js application.");
 console.log("God is great");
-console.log("I am practicing JavaScriptg")
+console.log("I am practicing JavaScript")
 // Function to add two numbers
 function addNumbers(a, b) {
     return a + b;
