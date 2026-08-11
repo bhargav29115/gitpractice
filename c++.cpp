93a1b35505c2e011bc6a787d91fa6bf0755f6e9d
unknown <pbhargavasai72@gmail.com>
2026-08-11 22:05:36 +0530

    Updated cpp file

--- c++.cpp
+++ c++.cpp
@@ -1,3 +1,11 @@
-for(int i=0;i<10;i++){
-    cout<<i*i+1<<endl;
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for(int i=0; i<10; i++){
+        cout << i*i+1 << endl;
+    }
+
+    return 0;
 }
